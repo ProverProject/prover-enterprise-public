13d2589317085b7c9f5dcf81ce4a122cb9e5efdb
Александр Соломатов <>
2019-03-05 09:06:05 +0300

    iOS: Fixed Mosaic namespace decoding

--- client/iOS/prover/Common/Common/Sources/BusinessLayer/Network/Responses/MosaicNamespace.swift
+++ client/iOS/prover/Common/Common/Sources/BusinessLayer/Network/Responses/MosaicNamespace.swift
@@ -1,6 +1,34 @@
 import Foundation
 
-public enum MosaicNamespace: String, Decodable {
+public enum MosaicNamespace: RawRepresentable, Decodable {
+    public typealias RawValue = String
+    
     case nem
     case prover
+    case other(String)
+    
+    public var rawValue: String {
+        switch self {
+        case .other(let namespace):
+            return namespace
+        case .nem:
+            return "nem"
+        case .prover:
+            return "prover"
+        }
+    }
+    
+    public init?(rawValue: String) {
+        switch rawValue {
+        case "nem": self = .nem
+        case "prover": self = .prover
+        default: self = .other(rawValue)
+        }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let namespace = try container.decode(String.self)
+        self = MosaicNamespace(rawValue: namespace) ?? .other(namespace)
+    }
 }
